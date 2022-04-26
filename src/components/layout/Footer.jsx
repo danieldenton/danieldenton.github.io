f9aee0cd6e63fba4d128e326e45f7c26bf2200e7
danieldenton <danielmdenton@gmail.com>
2022-04-26 14:33:11 -0700

    footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>© Daniel Denton 2022</p>
+      <p>© DD 2022</p>
       <div>
         <a
           target="_blank"
