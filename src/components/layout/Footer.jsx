678d4a5fb6fcf0678a7c7bba806f2f440eea1329
danieldenton <danielmdenton@gmail.com>
2022-04-28 15:26:10 -0700

    git hub up top

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,18 +1,8 @@
 export default function Footer() {
   return (
     <footer className="footer">
+      <p id="created">created in React.js</p>
       <p>© DD 2022</p>
-      <div>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/danieldenton"
-          className="github-link"
-        >
-          Github
-        </a>
-      </div>
-      <p id="created">portfolio created in React.js</p>
     </footer>
   );
 }
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -9,6 +9,16 @@ export default function Navbar() {
       <Link to="/about" className="navbar-link">
         About
       </Link>
+      <div>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/danieldenton"
+          className="navbar-link"
+        >
+          Github
+        </a>
+      </div>
       <div id="resume">
         <a className="resume-link" href="DanielDentonResume.pdf" download>
           download a resume
