0531607e9f4bdf90de1dde1a6f48d4d9ea2b7067
danieldenton <danielmdenton@gmail.com>
2022-05-02 10:58:35 -0700

    typo

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
       <h2 className="page-header">Bio</h2>
       <p className="bio">
         Daniel Denton is a software engineer who completed General Assembly's
-        Software Engineering Emmersive in 2022. He has a background as a working
+        Software Engineering Immersive in 2022. He has a background as a working
         musician as well as yoga and meditation teacher. Whether a blank canvas
         or an almost completed project, on a team or individually, he can be
         counted on to bring whatever creative piece is needed to complete the
