a082721cb0aa191433e6f9e00bb6a3c1a8a8d79a
danieldenton <danielmdenton@gmail.com>
2022-05-31 17:52:11 -0700

    about edit

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,12 +1,12 @@
 export default function About() {
   return (
     <p className="bio">
-      Daniel Denton is a software developer who completed General Assembly's
-      Software Engineering Immersive in 2022. Whether launching a project or
-      seeing it to completion, working with a team or independently, cumulative
-      skill-sets support quality outcomes with a drive to balance the technical
-      with the creative. He also has a background as a working musician as well
-      as a yoga and meditation teacher.
+      I am a software developer who completed General Assembly's Software
+      Engineering Immersive in 2022. Whether launching a project or seeing it to
+      completion, working with a team or independently, my skill-sets support
+      quality outcomes with a drive to balance the technical with the creative.
+      I also has a background as a working musician as well as a yoga and
+      meditation teacher.
     </p>
   );
 }
