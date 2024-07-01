bdbc13847e909e9d94af92ab7e46eda220bc3a6c
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-07-01 15:23:54 -0700

    new pn github links

--- src/components/PainNavigator.jsx
+++ src/components/PainNavigator.jsx
@@ -1,33 +1,67 @@
-import painNavImg from "../assets/PainNav.jpeg"
-
+import painNavImg from "../assets/PainNav.jpeg";
 
 export default function PainNavigator() {
-
-    const painNavigator = {
-        name: "PainNavigator",
-        image: painNavImg,
-        iOSLink: "https://apps.apple.com/us/app/painnavigator/id1631767340",
-        googlePlayLink: "https://play.google.com/store/apps/details?id=com.PainNavigator&hl=en_US",
-        mobileAppGithub: "https://github.com/danieldenton/deep-cut-discovery",
-        serverGithub: "https://github.com/danieldenton/deep-cut-discovery",
-        createdWith: "React Native, Ruby on Rails",
-    }
-    return (
-      <div className="painNavigator-div">
-        <h5 className="painNavigator-names">{painNavigator.name}</h5>
-        <p className="created-with">{painNavigator.createdWith}</p>
-        <div className="painNavigator-github">
-          <a
-            href={painNavigator.github}
-            target="_blank"
-            className="painNavigator-github-link"
-          >
-            <i class="fa-brands fa-github-square"></i>
-          </a>
+  const painNavigator = {
+    name: "PainNavigator",
+    image: painNavImg,
+    iOSLink: "https://apps.apple.com/us/app/painnavigator/id1631767340",
+    googlePlayLink:
+      "https://play.google.com/store/apps/details?id=com.PainNavigator&hl=en_US",
+    mobileAppGithub: "https://github.com/painnavigator/painnavigator-mobile",
+    serverGithub: "https://github.com/painnavigator/pain-navigator-mobile-api",
+    createdWith: "React Native, Ruby on Rails",
+  };
+  return (
+    <div className="painNavigator-div">
+      <h5 className="project-names">{painNavigator.name}</h5>
+      <p className="created-with">{painNavigator.createdWith}</p>
+      <div className="pn-halves">
+        <div className="pn-half-div">
+          <img
+            src={painNavigator.image}
+            alt="PainNavigator"
+            className="projects"
+          />
         </div>
-        <a href={painNavigator.link} target="_blank">
-          <img src={painNavigator.image} alt="A Day In The Life" className="painNavigators" />
-        </a>
+        <div className="pn-half-div">
+          <div className="painNavigator-github">
+            <a
+              href={painNavigator.mobileAppGithub}
+              target="_blank"
+              className="project-github-link"
+            >
+              <i class="fa-brands fa-github-square"></i>
+            </a>
+          </div>
+          <div className="painNavigator-github">
+            <a
+              href={painNavigator.serverGithub}
+              target="_blank"
+              className="project-github-link"
+            >
+              <i class="fa-brands fa-github-square"></i>
+            </a>
+          </div>
+          <div className="painNavigator-github">
+            <a
+              href={painNavigator.iOSLink}
+              target="_blank"
+              className="project-github-link"
+            >
+              <i class="fa-brands fa-app-store"></i>
+            </a>
+          </div>
+          <div className="painNavigator-github">
+            <a
+              href={painNavigator.googlePlayLink}
+              target="_blank"
+              className="project-github-link"
+            >
+              <i class="fa-brands fa-google-play"></i>
+            </a>
+          </div>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
