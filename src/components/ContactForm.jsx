9e93a48e991d712673ec9d1c49ae26b9a8494013
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-08-18 17:52:52 -0700

    form styling in progress

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -46,32 +46,58 @@ export const ContactForm = () => {
           ref={form}
           style={{ maxWidth: "450px", margin: "50px auto" }}
         >
-          <Form.Group className="mb-3" style={{ marginBottom: '10px'}} controlId="exampleForm.ControlInput1">
-            <Form.Label style={{ fontStyle: 'bold'}}>Name</Form.Label>
+          <Form.Group
+            className="mb-3"
+            style={{ marginBottom: "10px" }}
+            controlId="exampleForm.ControlInput1"
+          >
+            <Form.Label style={{ fontWeight: "bold" }}>Name</Form.Label>
             <Form.Control
               type="text"
               name="name"
-              style={{ width: "95%", height: "28px", borderRadius: "5px" }}
+              style={{
+                marginTop: "5px",
+                width: "95%",
+                height: "28px",
+                borderRadius: "5px",
+              }}
             />
           </Form.Group>
 
-          <Form.Group className="mb-3" style={{ marginBottom: '10px'}} controlId="exampleForm.ControlInput1">
-            <Form.Label>Email</Form.Label>
+          <Form.Group
+            className="mb-3"
+            style={{ marginBottom: "10px" }}
+            controlId="exampleForm.ControlInput1"
+          >
+            <Form.Label style={{ fontWeight: "bold", marginBottom: "7px" }}>
+              Email
+            </Form.Label>
             <Form.Control
               type="email"
               name="email"
-              style={{ width: "95%", height: "28px", borderRadius: "5px" }}
+              style={{
+                marginTop: "5px",
+                width: "95%",
+                height: "28px",
+                borderRadius: "5px",
+              }}
             />
           </Form.Group>
 
-          <Form.Group className="mb-3" style={{ marginBottom: '10px'}} controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Message</Form.Label>
+          <Form.Group
+            className="mb-3"
+            style={{ marginBottom: "10px" }}
+            controlId="exampleForm.ControlTextarea1"
+          >
+            <Form.Label style={{ fontWeight: "bold", marginBottom: "7px" }}>
+              Message
+            </Form.Label>
             <Form.Control
               type="text"
               name="message"
               as="textarea"
               rows={8}
-              style={{ width: "95%", borderRadius: '5px'}} 
+              style={{ marginTop: "5px", width: "95%", borderRadius: "5px" }}
             />
           </Form.Group>
 
@@ -79,10 +105,10 @@ export const ContactForm = () => {
             type="submit"
             className="fw-bold"
             style={{
-              backgroundColor: "white",
+              backgroundColor: "rgb(234, 255, 0)",
               width: "96%",
               height: "40px",
-              color: "purple",
+              color: "black",
               borderRadius: "7px",
               marginTop: "15px",
             }}
