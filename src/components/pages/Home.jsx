a7f2561906373f5d3f8071f0a6f6f194736c3569
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-08-31 14:25:06 -0700

    link fix

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,14 +1,13 @@
-
-
 export default function Home() {
   return (
     <div className="home">
       <h2>This is an old portfolio site. Visit my new portfolio:</h2>
       <a
+        className="new-link"
         rel="noreferrer"
         href="https://www.danieldentondev.com/"
       >
-        <h2>HERE</h2>
+        <h2> HERE</h2>
       </a>
     </div>
   );
